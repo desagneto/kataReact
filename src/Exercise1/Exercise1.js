7627655f8fc0b20d18dee24f0b6efb0bb6246a2e
Agneto <agneto@gmail.com>
2019-08-31 15:24:33 -0500

    deploy sh 5

--- src/Exercise1/Exercise1.js
+++ src/Exercise1/Exercise1.js
@@ -3,26 +3,14 @@ import React, {Component} from 'react';
 export class Exercise1 extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      board: ['a', 'b', 'c', 'd']
-    };
+    this.state = {};
   }
   
   render() {
-
-    const { board } = this.state;
-
     return (
-      <div>
-        {board.map((row, i) => (
-          <div key={i}>
-
-            <span >{row}</span>
-
-          </div>
-        ))}
+      <div className="container">
+        Exercise1 page
       </div>
-
     );
   }
 }
